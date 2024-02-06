bb5869e6a1e06cf2f3f93dfec683f5fdf635a3c0
SALIM UMER <salimbinumer@gmail.com>
2024-02-06 07:01:11 +0530

    Fixed the error
    35:

--- ja-mario/public/game.js
+++ ja-mario/public/game.js
@@ -1,4 +1,4 @@
-const {Client, Account, Database, ID, Queery } = Appwrite
+const {Client, Account, Database, ID, Query } = Appwrite
 
 const projectId= "65b6201e79e9c2ac1810"
 
@@ -8,27 +8,31 @@ const collectionId=''
 
 
 const client = new Client()
-.setEndpoint("https.//cloud.appwrite.io/v1")
+.setEndpoint("https://cloud.appwrite.io/v1")
 .setProject(projectId)
 
 
 
 const account = new Account(client)
 
-function register (event){
+function register(event){
     account.create(
         ID.unique(),
         event.target.elements['regsiter-email'].value,
         event.target.elements['register-password'].value,
-        event.target.elements['resgistered -usernane'].value
-
-    ).then(response => {
+        event.target.elements['resgistered-usernane'].value
+        ).then(response => {
         console.log(response)
 
         // createe a document in databasee
-    })
+        account.createEmailSession(
+            event.target.elements['register-email'].value,
+            event.target.elements['register-password'].value
+        )
+    }).catch(error => console.error(error))
+    event.preventDefault()
 
-        account.createEmailSession
+        
 
 }
 
